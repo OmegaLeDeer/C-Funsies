d62262c7d80ca2ef9d54d2e4d4046ca8da73bcad
DeerPad <alanfbjoao@gmail.com>
2025-04-25 20:16:12 +0100

    playin around

--- main.c
+++ main.c
@@ -2,5 +2,14 @@
 #include <stdlib.h>
 
 int main() { 
+
+    char name[50];
+
+    printf("Welcome to my C Program. What\'s your name?\n");
+
+    fgets(name, sizeof(name), stdin);
+
+    printf("Welcome, %s.", name);
+
     return 0;
 }
