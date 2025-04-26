31f0ee5a6e7fb7040fba19c74545863164c87dbf
DeerPad <alanfbjoao@gmail.com>
2025-04-26 17:57:53 +0100

    I'm tryinggggg

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-char *textParser(char *text);
+
+char *textParser(char **text, int length);
 
 int main() {
 
+    // Introduction 
+
     char name[50];
+
+    char *text[] = "lorem Ipsum sit amet dolor.";
 
     printf("Welcome to my C Program. What\'s your name?\n");
 
@@ -13,22 +20,37 @@ int main() {
 
     printf("Welcome, %s.\n", name);
 
-    printf("%s",textParser(name));
+    // Text Parsing
+
+    int length = strlen(text);
+
+    printf("%s",textParser(text, length));
 
     return 0;
 
 }
 
-char *textParser(char *text) {
+void *textParser(char **text, int length) {
 
-    if(text == NULL){
+    *text = malloc(length * sizeof(int));
+    if(*text == NULL){
 
-        return NULL;
+        return;
 
     }
 
+    printf("%s\n",text);
+
+    for (int i=0; i < length; i++) {
+
+        if(i>0 && isupper((*text)[i]) && (*text)[i-1] != " "){
+            printf("%s",newText[i]);
+        }
+
     
 
-    return text;
+    }
+    
+    return;
 
 }
